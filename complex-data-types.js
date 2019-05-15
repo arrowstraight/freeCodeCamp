4999ced4897e0487779abaaea13479746d2e45f8
arrowstraight <37030740+arrowstraight@users.noreply.github.com>
2019-05-15 12:56:48 +0100

    new completed test files on manipulating objects

---
+++ complex-data-types.js
@@ -0,0 +1,24 @@
+var myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [ 
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  },
+  {"artist": "ABBA",
+    "title": "Arrival",
+    "release_year": 1979,
+    "formats": [ 
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  }
+  // Add record here
+];
